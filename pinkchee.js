d184a1d3d39200a993040e1e7806b89d8252b7f1
Tturna <eemelijuponaho@gmail.com>
2023-06-26 10:30:40 +0300

    chee time zone for pink check

--- pinkchee.js
+++ pinkchee.js
@@ -1,4 +1,6 @@
 const { mulberry32 } = require('./customrandom.js');
+const { DateTime } = require('luxon');
+
 const PINKROLE = "1119327148411457657";
 const CHEEID = "535483016546615302";
 
@@ -20,12 +22,13 @@ module.exports = {
     },
 
     handlePinkChee: (guild) => {
-        const date = parseInt(new Date().toISOString().split('T')[0].replace(/-/g, ''));
+        const nyDate = DateTime.now().setZone('America/New_York');
+        const date = parseInt(nyDate.toISO().split('T')[0].replace(/-/g, ''));
         const rng = mulberry32(date);
         const shouldBePink = rng < 1/7;
 
         console.log("Chee pink check...");
-        console.log(`Date: ${date}, rng: ${rng}`);
+        console.log(`Date: ${nyDate}, rng: ${rng}`);
         console.log(`Chee should be pink: ${shouldBePink}`)
 
         guild.members.fetch(CHEEID)
