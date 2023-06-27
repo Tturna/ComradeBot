68c710763ff14f5c7109c39e1b2f72b1e82e91f1
Tturna <eemelijuponaho@gmail.com>
2023-06-27 23:18:33 +0300

    fixed daily interval from 22 minutes to hours lmao

--- economy.js
+++ economy.js
@@ -39,7 +39,7 @@ module.exports = {
 
                     const lastDailyBonusTime = data.lastDailyBonusTime ? data.lastDailyBonusTime : 0;
 
-                    if (nowUnix - lastDailyBonusTime > 22*60) {
+                    if (nowUnix - lastDailyBonusTime > 22*60*60) {
                         income += 100;
                     }
 
